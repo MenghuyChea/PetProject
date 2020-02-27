57443443be7e4c71d583da16efb69900218f3871
MenghuyChea <52589494+MenghuyChea@users.noreply.github.com>
2020-02-27 12:02:57 +0700

    change
    finish login and register

--- app/src/main/java/com/cheamenghuy/petproject/Login.java
+++ app/src/main/java/com/cheamenghuy/petproject/Login.java
@@ -1,7 +1,5 @@
 package com.cheamenghuy.petproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -14,11 +12,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Login extends AppCompatActivity {
 
     EditText name_login,pw_login;
     Button login_login;
     TextView reg_login;
+    private RequestQueue mRequestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,10 +76,11 @@ public class Login extends AppCompatActivity {
         }
         else
         {
-            Toast.makeText(this,"Username or Password not match",Toast.LENGTH_LONG).show();
-            name_login.setText("");
-            pw_login.setText("");
-            name_login.requestFocus();
+//            Toast.makeText(this,"Username or Password not match",Toast.LENGTH_LONG).show();
+//            name_login.setText("");
+//            pw_login.setText("");
+//            name_login.requestFocus();
+            Login1();
         }
     }
 
@@ -84,5 +100,45 @@ public class Login extends AppCompatActivity {
             return  user;
         }
         return null;
+    }
+    public void Login1(){
+        String url = "http://172.23.0.101/petproject1/login.php";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONObject jsonObject = new JSONObject(response);
+                    String success = jsonObject.getString("success");
+
+                    if(success.equals("1"))
+                    {
+                        Intent intent = new Intent(Login.this,MainApp.class);
+                        startActivity(intent);
+                    }else
+                    {
+                        Toast.makeText(getApplicationContext(),"Your Username or Password is wrong",Toast.LENGTH_LONG).show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"error: "+error.toString(),Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String,String>();
+                params.put("name",name_login.getText().toString().trim());
+                params.put("password",pw_login.getText().toString().trim());
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
--- app/src/main/java/com/cheamenghuy/petproject/Register.java
+++ app/src/main/java/com/cheamenghuy/petproject/Register.java
@@ -1,22 +1,38 @@
 package com.cheamenghuy.petproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
     EditText name_reg,email_reg,pw_reg,phone_reg;
     Button btn_reg_reg;
+    ProgressBar loading;
+    static String URL_REGIST = "http://172.23.0.101/petproject1/register.php";
     TextView btn_login_reg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,13 +82,15 @@ public class Register extends AppCompatActivity {
                 statement.bindString(4, phone);
                 statement.execute();
 
+                Register();
+
                 name_reg.setText("");
                 email_reg.setText("");
                 pw_reg.setText("");
                 phone_reg.setText("");
                 name_reg.requestFocus();
 
-                Intent intent = new Intent(Register.this, MainApp.class);
+                Intent intent = new Intent(Register.this, Login.class);
                 startActivity(intent);
             }
 
@@ -80,5 +98,50 @@ public class Register extends AppCompatActivity {
         {
             Toast.makeText(this,"Record Fail",Toast.LENGTH_LONG).show();
         }
+    }
+    public void Register(){
+        final String name = name_reg.getText().toString();
+        final String email = email_reg.getText().toString();
+        final String pass = pw_reg.getText().toString();
+        final String phone = phone_reg.getText().toString();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONObject jsonObject = new JSONObject(response);
+                    String success = jsonObject.getString("success");
+                    if(success.equals("1"))
+                    {
+                        Intent intent = new Intent(Register.this,Login.class);
+                        startActivity(intent);
+                    }else
+                    {
+                        Toast.makeText(getApplicationContext(),"Please fill all the information",Toast.LENGTH_LONG).show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"error: "+error.toString(),Toast.LENGTH_LONG).show();
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("name",name.trim());
+                params.put("email",email.trim());
+                params.put("password",pass.trim());
+                params.put("phonenumber",phone.trim());
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
